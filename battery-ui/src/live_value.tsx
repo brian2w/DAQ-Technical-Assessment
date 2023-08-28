a243505af60117161a1e0d6fc3a562640805cf46
Brian Wang <brian.wang@student.unsw.edu.au>
2023-08-28 15:28:55 +1000

    added italics

--- battery-ui/src/live_value.tsx
+++ battery-ui/src/live_value.tsx
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import './live_value.css';
 
 interface TemperatureProps {
   temp: number;
 }
 
-function LiveValue({ temp } : TemperatureProps) {
+function LiveValue({ temp }: TemperatureProps) {
+  const [isGlowing, setIsGlowing] = useState(true);
 
   let valueColour = 'white';
+  const red = Math.min(255, Math.max(0, temp));
+  const blue = Math.min(255, Math.max(0, 180 - temp));
 
   return (
-      <header className="live-value" style={{ color : valueColour }}>
-        {`${temp.toString()}°C`}
-      </header>
+    <header
+      className={`live-value`}
+      style={{
+        color: `rgb(${red}, 80, ${blue})`,
+        fontStyle: 'italic',
+        stroke: '',
+      }}
+    >
+      {`${temp.toString()}°C`}
+    </header>
   );
 }
 
